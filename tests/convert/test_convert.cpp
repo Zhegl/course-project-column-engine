3ee8a0e9098d7b59e1f03002bc757f779a63343a
Aleksandr Zheglov <a@zhegloff.ru>
2026-01-13 19:44:06 +0300

    Initial commit

---
+++ tests/convert/test_convert.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "convert.h"
+#include <glog/logging.h>
+
+TEST(ConvertTest, TestTest) {
+    Convert();
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1;
+
+    int result = RUN_ALL_TESTS();
+
+    google::ShutdownGoogleLogging();
+    return result;
+}

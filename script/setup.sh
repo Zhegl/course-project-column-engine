11415a8f47497974c2de48d51f71bf127c56cb1a
Aleksandr Zheglov <zhegl123@gmail.com>
2026-05-28 16:25:50 +0300

    a

--- script/setup.sh
+++ script/setup.sh
@@ -5,6 +5,5 @@ apt-get update
 apt-get install -y --no-install-recommends \
     cmake ninja-build \
     clang \
-    ca-certificates \
-    libre2-dev
+    ca-certificates
 rm -rf /var/lib/apt/lists/*
